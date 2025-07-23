948bc496cdf6caf2a5424b4e0abdb7ccb18f0bb8
Paulo Galarza <paulo-123@hotmail.es>
2025-07-23 11:52:31 -0500

    feat: Migracion para aumentar columna de recovery_token

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -17,6 +17,11 @@ const UserSchema = {
     type: DataTypes.STRING,
     allowNull: false,
   },
+  recoveryToken: {
+    field: 'recovery_token',
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
   role: {
     type: DataTypes.STRING,
     allowNull: false,
